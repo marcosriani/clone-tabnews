5a83ae07667e90fbc9d5bc672d0140391430e8bc
Marcos Riani <24310326+marcosriani@users.noreply.github.com>
2025-07-05 16:31:00 +0100

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      I will finish this project, from 0 to star, I will become the full stack
+      developer I always dreamed
+    </h1>
+  );
+}
+
+export default Home;
